9b1703cfc4cb6f88a5847059bc3c6d90f0c95d80
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-09 17:48:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+print("my name is hassen")
+print("I have created this in (3/9/20)")
+print("this Hello world is using pthon only")
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print("i have put value of the (x,y)")
+import math
+if 5 > 2:
+  print("is Five  greater than two!")
